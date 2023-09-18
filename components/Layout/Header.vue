062598aefe43b76a7d62a7f80527d19944cc5862
Sardorbek <s.jabborov@uic.group>
2023-09-18 17:48:22 +0500

    fix: logout btn

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-3 flex items-center justify-end bg-gray-700 border-l border-white">
     <p class="mr-6 text-white font-medium text-lg rounded-md px-2 border border-white">{{username}}</p>
-    <ButtonVButton @click="logOut" class="flex flex-row items-center gap-2 group">
-      <span>Log Out</span>
+    <ButtonVButton @click="logOut" class="flex flex-row items-center gap-2 group py-1">
+      <span>Chiqish</span>
       <IconsLogout class="h-6 w-6 text-white group-hover:text-blue-300 duration-300"/>
     </ButtonVButton>
   </div>
